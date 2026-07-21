0d71f75f63ef4de8160e859870ae953b46b3aea2
Vaishnavi B U <vaishnavi.b.u.1686@gmail.com>
2026-07-21 21:14:57 +0530

    Added Prime Number program

---
+++ prime_number.py
@@ -0,0 +1,14 @@
+import math
+num = int(input("Enter a number: "))
+if num < 2:
+    print("Not Prime")
+else:
+    is_prime = True
+    for i in range(2,int(math.sqrt(num))+1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print("Prime Number")
+    else:
+        print("Not Prime")
